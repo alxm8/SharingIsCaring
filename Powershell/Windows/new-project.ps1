6cb98a8dc820f5904275792726fcfb1ee44e9899
alxm8 <47351172+alxm8@users.noreply.github.com>
2021-01-02 11:56:59 +0100

    Update new-project.ps1

--- Powershell/Windows/new-project.ps1
+++ Powershell/Windows/new-project.ps1
@@ -1,63 +1,35 @@
-#creates a new project folder in your home user folder with the below structure
+#creates a credstore in your home user folder with the below structure
 
-#C:\USERS\USER\.CREDSTORE
-#├───bin
-#├───certs
-#├───keys
-#├───pscredentials
-#└───sshkeys
-#create .credstore in $home
-if ((Test-Path ($home + '\.credstore\')) -eq $false) {
-    mkdir ($home + '\.credstore\') #create credstore folder"
-    $path = ($home + '\.credstore\')
-    Write-Host "creating credstore in $path"
-    }else {
-        $path = ($home + '\.credstore\')
-        Write-Host "credstore already exists at path $path"
-
-}
-if ((Test-Path ($home + '\.credstore\certs\')) -eq $false) {
-    mkdir ($home + '\.credstore\certs\') #create credstore certs folder"
-    $path = ($home + '\.credstore\certs\')
-    Write-Host "creating credstore cert folder in $path"
-    }else {
-        $path = ($home + '\.credstore\certs\')
-        Write-Host "credstore cert folder already exists at path $path"
-
-}
-if ((Test-Path ($home + '\.credstore\keys\')) -eq $false) {
-    mkdir ($home + '\.credstore\keys\') #create credstore keys folder"
-    $path = ($home + '\.credstore\keys\')
-    Write-Host "creating credstore keys folder in $path"
-    }else {
-        $path = ($home + '\.credstore\keys\')
-        Write-Host "credstore keys folder already exists at path $path"
-
-}
-if ((Test-Path ($home + '\.credstore\pscredentials\')) -eq $false) {
-    mkdir ($home + '\.credstore\pscredentials\') #create credstore pscredential folder"
-    $path = ($home + '\.credstore\pscredentials\')
-    Write-Host "creating credstore pscredentials in $path"
-    }else {
-        $path = ($home + '\.credstore\pscredentials\')
-        Write-Host "credstore pscredentials already exists at path $path"
-
-}
-if ((Test-Path ($home + '\.credstore\bin\')) -eq $false) {
-    mkdir ($home + '\.credstore\bin\') #create credstore folder"
-    $path = ($home + '\.credstore\bin\')
-    Write-Host "creating credstore bin in $path"
-    }else {
-        $path = ($home + '\.credstore\bin\')
-        Write-Host "credstore bin already exists at path $path"
-
-}
-if ((Test-Path ($home + '\.credstore\sshkeys\')) -eq $false) {
-    mkdir ($home + '\.credstore\sshkeys\') #create credstore sshkeys folder"
-    $path = ($home + '\.credstore\sshkeys\')
-    Write-Host "creating credstore sshkeys in $path"
-    }else {
-        $path = ($home + '\.credstore\sshkeys\')
-        Write-Host "credstore sshkeys already exists at path $path"
-
+# tree /f
+#Folder PATH listing
+#C:.
+#└───mm-YYYY-tech-project
+#    │   tech-project-projectplan.md
+#    │
+#    ├───binaries
+#    ├───diagrams
+#    ├───documentation
+#    ├───scriptinputs
+#    ├───scriptoutputs
+#    └───scripts
+#create project folder
+if ((Test-Path ($home + '\projects\')) -eq $false) {
+    mkdir ($home + '\projects\') #create base project folder if it doesnt exist"
+    $path = ($home + '\projects\')
+    Write-Host "creating projects folder in $path"
+    }
+$date = get-date -Format "MM-yyyy"
+$projectname = read-host "enter the name of the project you want to create with the structure Technology-ProjectName for example AWS-CreatingInstances"
+$projectfoldername = ($date + '-' + $projectname)
+$projectfoldernamepath = ($home + '\projects\' + $projectfoldername)
+if ((Test-Path ($projectfoldernamepath)) -eq $false) {
+    mkdir ($projectfoldernamepath) #creates project folder"
+    Write-Host "creating project folders and files within $projectfoldernamepath"
+    mkdir -Path ($projectfoldernamepath + '\' + 'documentation')
+    mkdir -Path ($projectfoldernamepath + '\' + 'scripts')
+    mkdir -Path ($projectfoldernamepath + '\' + 'scriptoutputs')
+    mkdir -Path ($projectfoldernamepath + '\' + 'scriptinputs')
+    mkdir -Path ($projectfoldernamepath + '\' + 'diagrams')
+    mkdir -Path ($projectfoldernamepath + '\' + 'binaries')
+    New-Item -path $projectfoldernamepath  -Name  ($projectname + '-projectplan.md')
 }
