cc031081e0f194eb7be306129b1670af96074c2c
alxm8 <47351172+alxm8@users.noreply.github.com>
2021-01-03 12:16:48 +0100

    Update powershellprofilecheck.ps1

--- Powershell/v5/powershellprofilecheck.ps1
+++ Powershell/v5/powershellprofilecheck.ps1
@@ -1,1 +1,20 @@
+#checks powershell version is not v7 and creates the powershell profile file.
+
+if ($PSVersionTable -notlike 7) {
+
+    Write-Host "Powershell version is not 7"
+    if ((test-path $profile ) -eq $false) {
+
+        New-Item -Path $profile
+        Write-Host "creating powershell profile at $profile"
+        ii $profile
+        
+    }else {
+        Write-Host "profile file at $profile already exists"
+        ii $profile
+    }
+    
+}else {
+    write-host "Powershell Version $psversiontable.PSVersion is not compatible"
+}
 
