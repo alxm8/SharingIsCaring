e37c143ebf0b081f3ecb2840270e20bc1e082539
alxm8 <47351172+alxm8@users.noreply.github.com>
2021-01-03 12:29:52 +0100

    Update Microsoft.PowerShell_profile.ps1

--- Powershell/Profile/Microsoft.PowerShell_profile.ps1
+++ Powershell/Profile/Microsoft.PowerShell_profile.ps1
@@ -166,6 +166,21 @@ function Output-XifPpercentOfItIsY {
     $x = $Y /$P
     $x
 }
+function Swap-Gateway {
+    #swap default gateway from home router to VPN tunnel
+    $route = get-netroute -DestinationPrefix "0.0.0.0/0"
+    
+    if ($route.NextHop -eq "192.168.1.1") {
+        remove-netroute -ifindex $route.ifIndex -DestinationPrefix $route.DestinationPrefix -NextHop $route.NextHop -confirm:$false 
+        New-NetRoute -Ifindex $route.ifindex -DestinationPrefix $route.DestinationPrefix -NextHop "192.168.1.254" -confirm:$false
+    }else{
+        remove-netroute -ifindex $route.ifIndex -DestinationPrefix $route.DestinationPrefix -NextHop $route.NextHop -confirm:$false
+        New-NetRoute -Ifindex 9 -DestinationPrefix "0.0.0.0/0" -NextHop "192.168.1.1" -confirm:$false
+    }
+    Get-NetRoute -DestinationPrefix 0.0.0.0/0
+    Get-NetIPConfiguration | Where-Object IPv4DefaultGateway -ne $null
+        
+}
 #----Vars
 $profPublicIP = Get-PublicIP
 $profGitDIR = ($home + '\documents\git')
