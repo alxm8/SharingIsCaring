274213729ba06076090ed73a959ab4a2e1b3f493
alxm8 <47351172+alxm8@users.noreply.github.com>
2021-05-18 14:46:41 +0200

    Create installed-software.sh

---
+++ Kubernetes/Deployment/1.TemplateConfig/installed-software.sh
@@ -0,0 +1,2 @@
+sudo dnf install redhat-lsb-core #check version https://phoenixnap.it/kb/how-to-check-centos-version
+sudo dnf install ansible #ansible https://docs.ansible.com/ansible/latest/installation_guide/intro_installation.html
